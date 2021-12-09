072eab665d6f77ba9035848316f30d64dde7edc7
Karthik Valluri <karthikv722@gmail.com>
2021-12-09 12:36:13 -0800

    customizable colors about us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
+import requests, json
 
 # create a Flask instance
 from templates.aadya_aboutme_api import get_numberfact
@@ -36,7 +37,17 @@ def karthik():
 
 @app.route('/siya')
 def siya():
-    return render_template("siya.html")
+    url = "https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia"
+
+    headers = {
+        'x-rapidapi-host': "trivia-by-api-ninjas.p.rapidapi.com",
+        'x-rapidapi-key': "0a4557c36bmsh023bf219202e218p153360jsndbf67f2a9f06"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    output = json.loads(response.text)
+    print(response.text)
+    return render_template("siya.html", result = output)
 
 
 @app.route('/recipes')
@@ -109,6 +120,13 @@ def define():
         # print(result)
     return render_template("athena.html", result=result)
 
+@app.route('/about_us')
+def about_us():
+    return render_template("layouts/about_us.html")
+
+@app.route('/aboutustemp')
+def aboutustemp():
+    return render_template("/aboutustemp.html")
 
 # runs the application on the development server
 if __name__ == "__main__":
