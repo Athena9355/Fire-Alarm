f12334710313d5158fc614819c38cd11f9bc8959
siyadixit <dsiya006@gmail.com>
2021-12-09 21:55:11 -0800

    Updated About Me (Not Final)

---
+++ templates/siya_aboutme_api.py
@@ -0,0 +1,15 @@
+import requests
+
+def siya():
+
+    url = "https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia"
+
+    headers = {
+        'x-rapidapi-host': "trivia-by-api-ninjas.p.rapidapi.com",
+        'x-rapidapi-key': "0a4557c36bmsh023bf219202e218p153360jsndbf67f2a9f06"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+   # output = json.loads(response.text)
+    return response.text
+    #return render_template("siya.html", result = output)
