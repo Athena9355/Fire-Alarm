57f40537632fb5846fd4cb12c3cd5a7a74e01954
Athena9355 <gte782a@gmail.com>
2022-01-06 13:02:36 -0800

    with new api, the nutritional info api now works under athena.html. However, it doesn't work under nutritional page

--- templates/nutritional_info_api.py
+++ templates/nutritional_info_api.py
@@ -1,16 +1,35 @@
 import requests
 
 ##API source: https://rapidapi.com/edamam/api/edamam-food-and-grocery-database/
+
 def get_info(ingredient):
     ingredient = str(ingredient)
-    url = "https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
 
-    querystring = {"ingr": ingredient}
+    url = "https://calorieninjas.p.rapidapi.com/v1/nutrition"
+
+    querystring = {"query": ingredient}
 
     headers = {
-        'x-rapidapi-host': "edamam-food-and-grocery-database.p.rapidapi.com",
+        'x-rapidapi-host': "calorieninjas.p.rapidapi.com",
         'x-rapidapi-key': "53c0bdc20bmshd40bff3eddbed53p11401djsn6beee195a713"
     }
 
     information = requests.request("GET", url, headers=headers, params=querystring)
-    return information.text
+
+    return(information.text)
+
+
+
+#def get_info(ingredient):
+    #ingredient = str(ingredient)
+    #url = "https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
+
+    #querystring = {"ingr": ingredient}
+
+    #headers = {
+        #'x-rapidapi-host': "edamam-food-and-grocery-database.p.rapidapi.com",
+        #'x-rapidapi-key': "53c0bdc20bmshd40bff3eddbed53p11401djsn6beee195a713"
+    #}
+
+    #information = requests.request("GET", url, headers=headers, params=querystring)
+    #return information.text
