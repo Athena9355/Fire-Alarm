40aaaf5a2dc8ab684010fd74fcb1bef9b5f6c23b
Athena9355 <gte782a@gmail.com>
2022-01-15 08:52:25 -0800

    snake game page set up done

---
+++ templates/snake_game.py
@@ -0,0 +1,38 @@
+#https://www.youtube.com/watch?v=XGf2GcyHPhc
+import math
+import random
+import pygame
+import thinker as tk
+from tkthinker import message box
+class cube (object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        pass
+    def move(self, dirnx, dirny):
+        pass
+    def draw(self, surface, eyes=False):
+        pass
+class snake (object):
+    #snake is built up by the cube oject defined above
+    def __int__(self, color,pos):
+        pass
+    def move(self):
+        pass
+    def reset(self, pos):
+        pass
+    def addCube(self):
+        pass
+    def draw(self, surface):
+        pass
+def drawGrid(w, rows, surface):
+    pass
+def redrawWindow(surface):
+    pass
+def randomSnake(rows, items):
+    pass
+def message_box(subject, content):
+    pass
+def main():
+    pass
+
