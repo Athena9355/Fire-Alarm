7d37a70601d3e5633f4c934dbd345b2ea0ef1584
Athena9355 <gte782a@gmail.com>
2022-01-15 09:07:53 -0800

    created the outline of the box

--- templates/snake_game.py
+++ templates/snake_game.py
@@ -33,6 +33,22 @@ def randomSnake(rows, items):
     pass
 def message_box(subject, content):
     pass
+
 def main():
+    #purpose of main: set a main loop
+    #width and height --> dimension of grid
+    width = 500
+    height = 500
+    rows = 20 #number of rows. can be set to any number as long as it can be divided by the width and height evenly. i.e. 500 has to be the multiple of rows
+    win = pygame.display.set_mode ((width, height)) #making a surface
+    #setting up a snake object below
+
     pass
 
+rows =
+w =
+h =
+
+cube.rows = rows
+cube.w = w
+main()
